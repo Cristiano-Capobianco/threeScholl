350ecc0e5f662038493de6a3640804232b283392
Cristiano-Capobianco <cristiano.capobianco1986@gmail.com>
2023-06-01 17:46:39 +0200

    01-06-2023-JS

---
+++ 01-06-2023-JS/esercitazione/script.js
@@ -0,0 +1,52 @@
+// console.log("funziona");
+
+// var nome = "alice"; // "alice" = stringa (caratteri alfanumerici delimitatati tra gli apici)
+
+// var numero1 = 10; // numero INT (intero)
+// var pGreco = 3.14; // numero FLOAT (decimale)
+
+// var numero1 = 3 > 2;
+// console.log(numero1);
+
+// var number1 = 3 < 2;
+// var number2 = 1 < 2;
+// var number3 = true;
+
+// console.log(number1, number2, number3);
+// console.log(typeof number1, typeof number2, typeof number3);
+
+// var collezione = [1, 2, 3, 5, 6, 7, 8, 9];
+
+// var number4;
+// console.log(number4);
+// number4 = 10;
+// number4 = 20;
+// number4 += 5;
+// console.log(number4);
+
+// // CICLO IF
+
+// var number1 = 10;
+// if (number1 > 20) {
+//   console.log("evviva");
+// } else {
+//   console.log("paperino");
+// }
+
+// var number = prompt("inserisci un numero"); // prompt -> fa inserire il valore all'utente
+
+// if (number > 0) {
+//   console.log("stampa numero positivo");
+// } else if (number < 0) {
+//   console.log("stampa numero negativo");
+// } else {
+//   console.log(0);
+// }
+
+var numbery = prompt("dammi un numero");
+
+if (numbery % 2 == 0) {
+  console.log("numero pari");
+} else {
+  console.log("numero dispari");
+}
